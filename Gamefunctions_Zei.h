7d798a9bb704f054fe60a2cc815d444a7ba23d01
Zeitler Marc Alexander <s-mzeit1@haw-landshut.de>
2018-10-18 14:41:40 +0200

    header bearbeitet

--- Gamefunctions_Zei.h
+++ Gamefunctions_Zei.h
@@ -1,7 +1,10 @@
 #ifndef GAMEFUNCTIONS_ZEI_H
 #define GAMEFUNCTIONS_ZEI_H
 
-
-
+void spawn(); //Spawnen der neuen Teile im Spawnbereich
+int collision(); //Returnwert 1: Kollision, 0: keine Kollision 
+void reiheloeschen(); //Wenn eine Reihe voll ist, dann löschen
+int score(int); //Volle Reihe bedeutet 1 Punkt-> Aufrufparameter
+int highscore(int); //Returnwert 0: Fehler beim Speichern, 1: kein Fehler, Parameter ist score
 
 #endif
