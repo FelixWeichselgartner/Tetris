a58ae6df0827112047d518b79f8bd8a3e30e84ef
Zeitler Marc Alexander <s-mzeit1@haw-landshut.de>
2018-10-18 15:50:36 +0200

    Header und C Datei aktualisiert/geschrieben

--- Gamefunctions_Zei.h
+++ Gamefunctions_Zei.h
@@ -2,7 +2,9 @@
 #define GAMEFUNCTIONS_ZEI_H
 
 void spawn(); //Spawnen der neuen Teile im Spawnbereich
+void figcpy(char*, char*); //Funktion zum Kopieren von arrays
 int collision(); //Returnwert 1: Kollision, 0: keine Kollision 
+void change(); //Funktion zur Umwandlung der Steine von beweglich in fest
 void reiheloeschen(); //Wenn eine Reihe voll ist, dann löschen
 int score(int); //Volle Reihe bedeutet 1 Punkt-> Aufrufparameter
 int highscore(int); //Returnwert 0: Fehler beim Speichern, 1: kein Fehler, Parameter ist score
