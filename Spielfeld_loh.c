80f045aa7725f72452683d73cc01216785c35072
Matthias998 <matthias.lohberger@onlione.de>
2018-11-11 00:11:58 +0100

    neues Spielfeldlayout

--- Spielfeld_loh.c
+++ Spielfeld_loh.c
@@ -1,48 +1,88 @@
-#include <stdio.h>
-//#define ylength 26
-//#define xlength 10
-
-#define xlength 10
+ /*#include<stdio.h>
 #define ylength 26
+#define xlength 10 */
 
-#include "Gameloop_Wei.h"
 
 void ausgabe(){
 
-	printf("Tetris\n");
+	printf("\n\n         TETRIS\n\n");
 	
 	int a, b;								//Zählvariable
-	int c = xlength + 2;							// Spaltenanzahl rand = 12 --> 12 x _ + Endenull
+	int c = xlength + 2;					// Spaltenanzahl rand = 12 --> 12 x _ + Endenull
+
+	char d = 186;		//Hochteil
+	char e = 187;		//Eckteil rechts oben
+	char f = 188;		//Eckteil rechts unten
+	char g = 200;		//Eckteil links unten
+	char h = 201;		//Eckteil links oben
+	char k = 202;		//_|_ Verbinder
+	char l = 203;		//anderer Verbinder
+	char m = 205;		//Querteil
+	char n = 185;		//Verbinder rechte Seite
+	char p = 204;		//Verbinder linke Seite
+
+	char spielfeld[ylength][xlength];		//Spielfelarray
 	 
+
 	for (a = 0; a <= ylength -1; a++) {		//Spielfeld befüllen
 		for (b = 0; b <= xlength-1; b++) {
 			spielfeld[a][b] = ' ';
 		}
 	}
-								 
+			
+
+			
 	//Ausgabe Spielfeld
-	for (a = 0; a <=c; a++) {				// Ausgabe oberer Rand    
-		printf("_");
+
+	printf("   %c", h);
+	for (a = 1; a <= 16; a++) {				// Ausgabe oberer Rand next
+		printf("%c", m);
 	}
-	printf("\n");
+	printf("%c\n", e);
+	printf("   %c   Next:        %c\n", d, d);	//Next Teil
+	for (a = 1; a <= 3; a++) {
+		printf("   %c                %c\n", d, d);
+	}
+	 
+	printf("   %c%c%c%c", g, m, m, l);
+	for (a = 3; a <=c; a++) {				// Ausgabe oberer Rand Spielfeld    
+		printf("%c", m);
+	}
+	printf("%c%c%c%c\n", l, m, m, f);
+
 	for (a = 4; a <= ylength-1; a++) {		// Ausgabe Zeile 4, 4 obere Zeilen = Spawn -> werden nicht ausgegeben
-		printf("|");
+		printf("      %c", d);
 		for (b = 0; b <= xlength-1; b++) {
 			printf("%c", spielfeld[a][b]);
 		}
-		printf("|\n");
+		printf("%c\n", d);
 	}
-	for (a = 0; a <= c; a++) {				 // Ausgabe unterer Rand
-		printf("_");
+
+	printf("   %c%c%c", h, m, m);
+	printf("%c", k);
+	for (a = 3; a <= c; a++) {				 // Ausgabe unterer Rand Spielfeld
+		printf("%c", m);
 	}										 
-											 
-	printf("\n");
-	printf("Ende\n");
+	printf("%c%c%c%c\n", k, m, m, e);		//Score Teil
+	printf("   %c  Score:        %c\n", d, d);
+	printf("   %c                %c\n", d, d);
+	printf("   %cHighscore:      %c\n", d, d);
+	printf("   %c", p);						//unterer Rand Score Teil
+	for (a = 1; a <= 16; a++) {				// Ausgabe oberer Rand next
+		printf("%c", m);
+	}
+	printf("%c\n", n);
+	printf("   %cName:           %c\n", d, d);	//Spielername
+	printf("   %c", g);
+	for (a = 1; a <= 16; a++) {				// Ausgabe unterer Rand Name
+		printf("%c", m);
+	}
+	printf("%c\n", f); 
 }
 
+ 
 
-
-/*int main() {
+/* int main() {
 	ausgabe();
 	getch();
 } */
