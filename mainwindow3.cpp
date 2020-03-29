9cbc259ca39254db2ef84cd70889b82c6e3fbd32
Marc <zeitlermarc2@gmail.com>
2020-03-29 12:27:36 +0200

    Neues Multiplayer-Window

---
+++ mainwindow3.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow3.h"
+#include "ui_mainwindow3.h"
+
+MainWindow3::MainWindow3(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow3)
+{
+    ui->setupUi(this);
+}
+
+MainWindow3::~MainWindow3()
+{
+    delete ui;
+}
+
+void MainWindow3::on_pshExit_clicked()
+{
+    close();
+}
