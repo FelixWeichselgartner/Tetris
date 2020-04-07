b429490ced33714c9b7cdd4d134d78954dca46ea
felix <felix@linux.fritz.box>
2020-04-07 17:12:33 +0200

    added update of window

--- mainwindow2.cpp
+++ mainwindow2.cpp
@@ -28,6 +28,7 @@ void MainWindow2::on_pshExit_clicked()
 void MainWindow2::paintEvent(QPaintEvent *event) {
     QPainter painter(this);
     draw_field(&painter, this->tetris.field);
+    this->update();
 }
 
 void MainWindow2::on_pushButton_clicked()
